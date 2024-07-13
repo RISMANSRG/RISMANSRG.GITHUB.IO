559c074fea5bc5626d1c6c5f4f4cb4dc0f88fea8
RISMANSRG <rismansaragih27@gmail.com>
2024-07-13 17:05:32 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1720864352,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/pesawat-sheet0.png",
+		"images/batu1-sheet0.png",
+		"images/laser-sheet0.png",
+		"images/object-sheet0.png",
+		"images/particles.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
